b5e8ce010bb38bad7ec1d6f46f7df2e01db5c5ec
Saman Ashoori <saman@samanashoori.com>
2025-08-29 00:06:32 +0100

    Added Enemy Movement + Death
    The robot enemy can now move with the walking animation + can kill you on contact

--- Scenes/character_body_2d.gd
+++ Scenes/character_body_2d.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -500.0
 const ACCELERATION = 0.1
 const DECELERATION = 0.1
 
--- Scenes/enemy.gd
+++ Scenes/enemy.gd
@@ -1,1 +1,32 @@
 extends CharacterBody2D
+
+var moving_right = 1
+const SPEED = 25
+var canSwitch = true
+
+func _physics_process(delta: float) -> void:
+	
+	
+	if !$RayCast2D.is_colliding() and canSwitch:
+		moving_right *= -1
+		canSwitch = false
+	else:
+		canSwitch = true
+	
+	if moving_right < 0:
+		velocity.x = SPEED * -1
+		$AnimatedSprite2D.flip_h = true
+		$RayCast2D.target_position = Vector2(-50,50)
+	else:
+		velocity.x = SPEED * 1
+		$AnimatedSprite2D.flip_h = false
+		$RayCast2D.target_position = Vector2(50,50)
+		
+	$AnimatedSprite2D.play("walk")
+	move_and_slide()
+	
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group('player'):
+		body.kill_player() # Replace with function body.
