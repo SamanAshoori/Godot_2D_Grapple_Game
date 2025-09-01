87321a63977cdbc02cc8f1e77fb8291718654e16
Saman Ashoori <saman@samanashoori.com>
2025-09-01 23:44:13 +0100

    made gun remove enemy
    gun now kills enemy

--- Scenes/bullet.gd
+++ Scenes/bullet.gd
@@ -8,3 +8,8 @@ func _process(delta: float) -> void:
  
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	area.get_parent().queue_free()
+	queue_free() # Replace with function body.
