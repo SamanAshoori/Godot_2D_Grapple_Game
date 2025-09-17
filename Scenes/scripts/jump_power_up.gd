6e259cc50ec87b62cf2eda739083143d3aba771c
Saman Ashoori <saman@samanashoori.com>
2025-09-17 22:52:51 +0100

    added more input options

--- Scenes/scripts/jump_power_up.gd
+++ Scenes/scripts/jump_power_up.gd
@@ -7,11 +7,15 @@ func _ready() -> void:
 
 func play_floating_animation() -> void:
 	var tween := create_tween()
+	# Optional: make the animation a bit smoother
 	tween.set_trans(Tween.TRANS_SINE)
+	tween.set_ease(Tween.EASE_IN_OUT)
 
-	var sprite_2d := get_node('.')
 	var position_offset := Vector2(0.0, 4.0)
-	var duration = randf_range(0.8, 1.2)
-	tween.tween_property(sprite_2d, "position", position_offset, duration)
-	tween.tween_property(sprite_2d, "position",  -1.0 * position_offset, duration)
+	var duration := randf_range(0.8, 1.2)
+
+	# Chain .as_relative() to treat the position_offset as a relative move
+	tween.tween_property(self, "position", position_offset, duration).as_relative()
+	tween.tween_property(self, "position", -position_offset, duration).as_relative()
+	
 	tween.set_loops()
